9aa5f44268f74154e6385cc705ae53b61e569899
oguzcoll <oguzcolak2000@gmail.com>
2023-09-25 21:43:31 +0300

    api side sorting of bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,13 +12,20 @@ export function useBookings() {
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+
+  //sort
+
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
