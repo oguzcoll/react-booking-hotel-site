1b4c5558a920ad9e5a328fcaf5f1ba3dfcba4802
oguzcoll <oguzcolak2000@gmail.com>
2023-09-26 19:20:44 +0300

    implemented authantication and authorization

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,29 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { login } from "../../services/apiAuth";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("mauro@gmail.com");
+  const [password, setPassword] = useState("icardi");
 
-  function handleSubmit() {}
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -18,6 +35,7 @@ function LoginForm() {
           id="email"
           // This makes this form better for password managers
           autoComplete="username"
+          disabled={isLoading}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -27,12 +45,15 @@ function LoginForm() {
           type="password"
           id="password"
           autoComplete="current-password"
+          disabled={isLoading}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? " Login" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
