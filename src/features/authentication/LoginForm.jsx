1a7a7efd36955ee9576f704acb729da87dd6ae24
oguzcoll <oguzcolak2000@gmail.com>
2023-09-27 18:17:52 +0300

    last version

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("ogz@gmail.com");
+  const [password, setPassword] = useState("123456789");
 
   const { login, isLoading } = useLogin();
 
