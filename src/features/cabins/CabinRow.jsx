16626c8daf03dccf733e7e9ff274e2cb05a50aa3
oguzcoll <oguzcolak2000@gmail.com>
2023-09-27 18:05:33 +0300

    bux fixing complete

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -96,6 +96,7 @@ function CabinRow({ cabin }) {
                 <Menus.Button
                   icon={<HiSquare2Stack />}
                   onClick={handleDublicate}
+                  disabled={isCreating}
                 >
                   Dublicate
                 </Menus.Button>
